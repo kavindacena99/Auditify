46db47905a1e25cee1dd9f8a813c1e8d300aa0e2
kavinda <kavindach4@gmail.com>
2025-09-18 21:07:20 +0530

    Initialize backend with Express, MongoDB, and necessary dependencies

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+
+dotenv.config(); // load env variables
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// connect to mongodb
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log("MongoDB Error", err));
+
+// routes
+
+// server starts
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
